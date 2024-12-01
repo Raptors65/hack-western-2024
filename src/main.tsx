f0c594c12ce62d7d4a862d710eef0b7a1c7084b2
Ching Lam Lau <contact.minicube11@gmail.com>
2024-11-30 21:45:49 -0500

    Add Auth0 for login

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Auth0Provider } from '@auth0/auth0-react';
 import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
@@ -12,7 +13,15 @@ const basename = import.meta.env.BASE_URL;
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter basename={basename}>
-      <App />
+      <Auth0Provider
+        domain="dev-rurkrs61sgsd8a4g.us.auth0.com"
+        clientId="3mG1v3LZJTEcRqGX1GWzK45spY1HR3Fl"
+        authorizationParams={{
+          redirect_uri: "http://localhost:5173"
+        }}
+      >
+        <App />
+      </Auth0Provider>
     </BrowserRouter>
   </React.StrictMode>,
 );
